f5381830f2d5de93260990cc0f6fd13766bd2fc7
Kyle Guerrero <kyleguerrero85@gmail.com>
2020-09-09 15:29:23 -0500

    feat: created a Loan model
    created a basic model for loans that will be used
    for a internal representation of a loan.

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -1,4 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AcmeApi.Models
 {
-
+        public class Loan
+    {
+        public int Id { get; set; }
+        public string applicantName { get; set; }
+        public string applicantPhoneNumber { get; set; }
+        public string applicantEmail { get; set; }
+        public string loanAmount { get; set; }
+        public string loanId { get; set; }
+        
+    }
 }
