6abd1d11a514df866f34ed32a8d8e3147b4e1e5e
Kyle Guerrero <kyleguerrero85@gmail.com>
2020-09-09 20:15:36 -0500

    chore: added data annotations
    added data annotations to all the fields so
    that the migrations generated would reflect
    constraints that we want in the DB

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -4,11 +4,28 @@ namespace AcmeApi.Models
 {
         public class Loan
     {
+
+        [Key]
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(250)]
         public string applicantName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string applicantPhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string applicantEmail { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string loanAmount { get; set; }
+
+        [Required]
+        [MaxLength(300)]
         public string loanId { get; set; }
         
     }
