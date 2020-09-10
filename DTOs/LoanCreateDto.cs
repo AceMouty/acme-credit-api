ba02988306fa2f5304ba7f4e1d8c7cf02cf23c08
Kyle Guerrero <kyleguerrero85@gmail.com>
2020-09-09 22:12:41 -0500

    feat: created LoanCreatedDto
    created dto for handling incoming data from
    a external client, this DTO is for incoming
    external representation of what we expect to
    be sent to the API when a client wants
    to create a new loan

---
+++ DTOs/LoanCreateDto.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AcmeApi.DTOs
+{
+    public class LoanCreateDto
+    {
+        
+        [Required]
+        public string applicantName { get; set; }
+
+        [Required]
+        public string applicantPhoneNumber { get; set; }
+
+        [Required]
+        public string applicantEmail { get; set; }
+
+        [Required]
+        public string loanAmount { get; set; }
+
+        [Required]
+        public string loanId { get; set; }
+    }
+}
