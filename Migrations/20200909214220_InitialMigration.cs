40e5f63ed75d39bd61e2372c7df9db62f2725712
Kyle Guerrero <kyleguerrero85@gmail.com>
2020-09-09 20:14:26 -0500

    feat: created initial migrations
    created migrations for setting up the loans table
    and also for seeding the loans table. migrations
    are working as expected

---
+++ Migrations/20200909214220_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmeApi.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Loans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    applicantName = table.Column<string>(maxLength: 250, nullable: false),
+                    applicantPhoneNumber = table.Column<string>(maxLength: 250, nullable: false),
+                    applicantEmail = table.Column<string>(maxLength: 250, nullable: false),
+                    loanAmount = table.Column<string>(maxLength: 250, nullable: false),
+                    loanId = table.Column<string>(maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Loans", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Loans");
+        }
+    }
+}
---
+++ Migrations/20200910003220_InitialSeed.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmeApi.Migrations
+{
+    public partial class InitialSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Loans",
+                columns: new[] { "Id", "applicantEmail", "applicantName", "applicantPhoneNumber", "loanAmount", "loanId" },
+                values: new object[] { 1, "JohnDoe@gmail.com", "John Doe", "000-867-5309", "34,000.00", "924e075f-4058-49fb-98f4-11283a1c2ad2" });
+
+            migrationBuilder.InsertData(
+                table: "Loans",
+                columns: new[] { "Id", "applicantEmail", "applicantName", "applicantPhoneNumber", "loanAmount", "loanId" },
+                values: new object[] { 2, "JaneDoe@gmail.com", "Jane Doe", "000-867-5310", "80,000.00", "0d7f34f6-f9ef-4077-b1f1-925a7a0bc884" });
+
+            migrationBuilder.InsertData(
+                table: "Loans",
+                columns: new[] { "Id", "applicantEmail", "applicantName", "applicantPhoneNumber", "loanAmount", "loanId" },
+                values: new object[] { 3, "JimmyDoe@gmail.com", "Jimmy Doe", "000-867-5311", "200.00", "71da68a7-c1e4-4497-9fb4-29b2bd8acfca" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Loans",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Loans",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Loans",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
